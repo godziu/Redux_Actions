2a2c4c5b2ac79013d143027f7cabf4881a2154e9
Damian Godek <damian.godek1@gmail.com>
2018-11-27 13:50:48 +0100

    add actions

---
+++ actions.js
@@ -0,0 +1,45 @@
+import uuid from uuid;
+
+const REMOVE_COMMENT = 'REMOVE_COMMENT';
+const EDIT_COMMENT = 'EDIT_COMMENT';
+const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+
+function createComment(text) {
+    return {
+        type: CREATE_COMMENT,
+        text,
+        id: uuid.v4()
+    }
+}
+
+function editComment(id) {
+    return {
+        type: EDIT_COMMENT,
+        text: 'Wyedytowany tekst komentarza',
+        id
+    }
+}
+
+function deleteComment(id) {
+    return {
+        type: REMOVE_COMMENT,
+        id
+    }
+}
+
+function thumbUpComment(votes, id) {
+    return {
+        type: THUMB_UP_COMMENT,
+        id,
+        votes: votes++
+    }
+}
+
+function thumbDownComment(votes, id) {
+    return {
+        type: THUMB_DOWN_COMMENT,
+        id,
+        votes: votes--
+    }
+}
